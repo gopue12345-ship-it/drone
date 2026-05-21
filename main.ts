52418c252418a37c7fb32d64884b85b074ce9c93
gopue12345-ship-it <gopue12345@gmail.com>
2026-05-21 20:36:38 +0200

    used to use a motor witg your microbit

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    RingbitCar.turnleft()
 })
+input.onGesture(Gesture.ScreenUp, function () {
+    RingbitCar.back()
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    RingbitCar.forward()
+})
+input.onButtonPressed(Button.B, function () {
+    RingbitCar.turnright()
+})
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+basic.pause(100)
